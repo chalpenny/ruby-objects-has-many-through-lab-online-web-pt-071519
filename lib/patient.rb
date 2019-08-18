9247fd96daacd19da56821be319952bf0ed6d078
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-18 03:11:26 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,8 @@ class Patient
   end
 
   def appointments
-     Appointment.all.select { |appointment| appointment.patient == self }
+     Appointment.all.select do |x| x.patient == self 
+     end
    end
 
   def doctors
